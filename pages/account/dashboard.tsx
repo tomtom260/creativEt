b5999a9f9dea4c50cd54536ae942faec4b600ce8
tomtom260 <thomas.mesfin@etmsoftwareplc.com>
2022-06-19 19:24:56 +0300

    fix:empty dashboard error handled

--- pages/account/dashboard.tsx
+++ pages/account/dashboard.tsx
@@ -75,129 +75,140 @@ function Dashboard({
 
   return (
     <DefaultLayout>
-      <div className="grid grid-cols-4">
-        <div className="col-span-4 md:col-span-3">
-          <Stats stats={performanceStats} />
-          <div className="mt-12 ">
-            <div className="flex md:flex-row items-center flex-col gap-12 overflow-x-auto pb-16">
-              <div className="">
-                <Text className="mb-4" varaint={TypographyVariant.Body1}>
-                  Revenue per day
-                </Text>
-                <AreaChart
-                  stock={groupedRevenue}
-                  width={isSmallScreen ? 300 : 500}
-                  height={200}
-                />
-              </div>
-              <div>
-                <Text
-                  className="mb-4 whitespace-nowrap"
-                  varaint={TypographyVariant.Body1}
-                >
-                  Revenue Distribution
-                </Text>
-                <PieChart
-                  distribution={revenueDistribution}
-                  width={300}
-                  height={200}
-                />
-              </div>
-            </div>
-          </div>
+      {!mostViewedContent ? (
+        <div className="flex justify-center items-center mt-10">
+          <Text varaint={TypographyVariant.H1}>
+            Upload A Content To see Statistics
+          </Text>
         </div>
-        <div className=" hidden xl:block pl-12 py-8 ">
-          <div className="flex-col flex justify-center items-center">
-            <div className="relative w-20 md:w-32 h-20 md:h-32 rounded-full overflow-hidden">
-              <ImageWithSkeleton layout="fill" src={user?.image!} />
-            </div>
-            <Text className="mt-6 mb-2" varaint={TypographyVariant.H2}>
-              {user?.name}
-            </Text>
-            <Text className="text-gray-dark " varaint={TypographyVariant.Body2}>
-              {user?.bio}
-            </Text>
-            <div className="flex justify-evenly  w-full mt-8">
-              {Object.keys(profileStats).map((stat) => (
-                <div key={stat} className="flex flex-col items-center">
-                  <Text varaint={TypographyVariant.H2}>
-                    {profileStats[stat]}
+      ) : (
+        <div className="grid grid-cols-4">
+          <div className="col-span-4 md:col-span-3">
+            <Stats stats={performanceStats} />
+            <div className="mt-12 ">
+              <div className="flex md:flex-row items-center flex-col gap-12 overflow-x-auto pb-16">
+                <div className="">
+                  <Text className="mb-4" varaint={TypographyVariant.Body1}>
+                    Revenue per day
                   </Text>
-                  <Text
-                    className="text-gray-dark "
-                    varaint={TypographyVariant.Body2}
-                  >
-                    {stat}
-                  </Text>
+                  <AreaChart
+                    stock={groupedRevenue}
+                    width={isSmallScreen ? 300 : 500}
+                    height={200}
+                  />
                 </div>
-              ))}
-            </div>
-            <div className="flex gap-4 my-12">
-              <Button
-                onClick={() => {
-                  router.push(`/${user?.username}`)
-                }}
-                variant={ButtonVariants.PRIMARY}
-              >
-                View Profile
-              </Button>
-              <Button
-                onClick={() => {
-                  router.push("/account/Profile")
-                }}
-                variant={ButtonVariants.OUTLINED}
-              >
-                Edit Profile
-              </Button>
+                <div>
+                  <Text
+                    className="mb-4 whitespace-nowrap"
+                    varaint={TypographyVariant.Body1}
+                  >
+                    Revenue Distribution
+                  </Text>
+                  <PieChart
+                    distribution={revenueDistribution}
+                    width={300}
+                    height={200}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="pb-16 col-span-4 flex md:flex-row  flex-col gap-4 md:gap-12 overflow-x-auto my-4 md:my-12 items-center md:items-end ">
-          <div className="  ">
-            <Text className="mb-4" varaint={TypographyVariant.Body1}>
-              Likes per day
-            </Text>
-            <AreaChart
-              stock={groupedLikes}
-              width={isSmallScreen ? 300 : 500}
-              height={200}
+          <div className=" hidden xl:block pl-12 py-8 ">
+            <div className="flex-col flex justify-center items-center">
+              <div className="relative w-20 md:w-32 h-20 md:h-32 rounded-full overflow-hidden">
+                <ImageWithSkeleton layout="fill" src={user?.image!} />
+              </div>
+              <Text className="mt-6 mb-2" varaint={TypographyVariant.H2}>
+                {user?.name}
+              </Text>
+              <Text
+                className="text-gray-dark "
+                varaint={TypographyVariant.Body2}
+              >
+                {user?.bio}
+              </Text>
+              <div className="flex justify-evenly  w-full mt-8">
+                {Object.keys(profileStats).map((stat) => (
+                  <div key={stat} className="flex flex-col items-center">
+                    <Text varaint={TypographyVariant.H2}>
+                      {profileStats[stat]}
+                    </Text>
+                    <Text
+                      className="text-gray-dark "
+                      varaint={TypographyVariant.Body2}
+                    >
+                      {stat}
+                    </Text>
+                  </div>
+                ))}
+              </div>
+              <div className="flex gap-4 my-12">
+                <Button
+                  onClick={() => {
+                    router.push(`/${user?.username}`)
+                  }}
+                  variant={ButtonVariants.PRIMARY}
+                >
+                  View Profile
+                </Button>
+                <Button
+                  onClick={() => {
+                    router.push("/account/Profile")
+                  }}
+                  variant={ButtonVariants.OUTLINED}
+                >
+                  Edit Profile
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="pb-16 col-span-4 flex md:flex-row  flex-col gap-4 md:gap-12 overflow-x-auto my-4 md:my-12 items-center md:items-end ">
+            <div className="  ">
+              <Text className="mb-4" varaint={TypographyVariant.Body1}>
+                Likes per day
+              </Text>
+              <AreaChart
+                stock={groupedLikes}
+                width={isSmallScreen ? 300 : 500}
+                height={200}
+              />
+            </div>
+            <Card
+              title="Highest Grossing Content"
+              name={mostSoldContent.title}
+              image={mostSoldContent.image}
+              label="ETB"
+              value={mostSoldContent.revenue}
+            />
+            <Card
+              title="Most Liked Content"
+              name={mostLikedContent.title}
+              image={mostLikedContent.image}
+              label="likes"
+              value={mostLikedContent._count.likes}
             />
           </div>
-          <Card
-            title="Highest Grossing Content"
-            name={mostSoldContent.title}
-            image={mostSoldContent.image}
-            label="ETB"
-            value={mostSoldContent.revenue}
-          />
-          <Card
-            title="Most Liked Content"
-            name={mostLikedContent.title}
-            image={mostLikedContent.image}
-            label="likes"
-            value={mostLikedContent._count.likes}
-          />
-        </div>
-        <div className="pb-16 col-span-4 flex md:flex-row flex-col gap-4  md:gap-14 overflow-x-auto items-center md:items-end  ">
-          <div className="mt-12  ">
-            <Text className="mb-4" varaint={TypographyVariant.Body1}>
-              Views per day
-            </Text>
-            <AreaChart
-              stock={groupedViews}
-              width={isSmallScreen ? 300 : 500}
-              height={200}
+          <div className="pb-16 col-span-4 flex md:flex-row flex-col gap-4  md:gap-14 overflow-x-auto items-center md:items-end  ">
+            <div className="mt-12  ">
+              <Text className="mb-4" varaint={TypographyVariant.Body1}>
+                Views per day
+              </Text>
+              <AreaChart
+                stock={groupedViews}
+                width={isSmallScreen ? 300 : 500}
+                height={200}
+              />
+            </div>
+            <Card
+              title="Most Viewed Content"
+              name={mostViewedContent.title}
+              image={mostViewedContent.image}
+              label="views"
+              value={mostViewedContent._count.View}
             />
           </div>
-          <Card
-            title="Most Viewed Content"
-            name={mostViewedContent.title}
-            image={mostViewedContent.image}
-            label="views"
-            value={mostViewedContent._count.View}
-          />
         </div>
-      </div>
+      )}
     </DefaultLayout>
   )
 }
